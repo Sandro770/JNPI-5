56ecb54a67d6e365c3015b8bce560331c6a28e20
Alessandro <alessandroperia@icloud.com>
2022-12-09 12:07:23 +0100

    Add constructors

--- kvfifo.h
+++ kvfifo.h
@@ -4,17 +4,49 @@
 #include <concepts>
 
 template <typename K, typename V> class kvfifo{
-  struct k_iterator requires std::bidirectional_iterator<kvfifo<K,V>> {
-  
-  };
-  kvfifo();
-  kvfifo(kvfifo const &);
-  kvfifo(kvfifo &&);
-  kvfifo& operator=(kvfifo other);
-  void push(K const &k, V const &v);
-  void pop();
-  void pop(K const &);
-  void move_to_back(K const &k);
+public:
+  struct k_iterator requires std::bidirectional_iterator<kvfifo<K, V>>;
+  kvfifo() {
+    queue = std::make_shared<queue_t>();
+    k_to_iterators = std::make_shared<k_to_iterators_t>();
+  }
+
+  kvfifo(kvfifo const &other) {
+    queue = other.queue;
+    k_to_iterators = other.k_to_iterators;
+  }
+
+  kvfifo(kvfifo &&other) {
+    queue = std::move(other.queue);
+    k_to_iterators = std::move(other.k_to_iterators);
+  }
+
+  kvfifo& operator=(kvfifo other) { 
+    queue = std::move(other.queue);
+    k_to_iterators = std::move(other.k_to_iterators);
+    return *this;
+  }
+
+  void push(K const &k, V const &v) {
+    modify();
+
+  }
+
+  void pop() {
+    modify();
+
+  }
+
+  void pop(K const &) {
+    modify();
+
+  }
+
+  void move_to_back(K const &k) {
+    modify();
+
+  }
+
   std::pair<K const &, V &> front();
   std::pair<K const &, V const &> front() const;
   std::pair<K const &, V &> back();
@@ -29,6 +61,20 @@ template <typename K, typename V> class kvfifo{
   void clear();
   k_iterator k_begin();
   k_iterator k_end();
+private:
+  using queue_t = std::list<std::pair<K, V>>;
+  using k_to_iterators_t = std::map<K, std::queue<queue_t::iterator>>;
+
+  std::shared_ptr<queue_t> queue;
+  std::shared_ptr<k_to_iterators_t> k_to_iterators;
+
+  void modify() {
+    if (queue.use_count() > 1 || k_to_iterators.use_count() > 1) { /// create a copy
+      queue = std::make_shared<queue_t>(queue_t(*queue));
+      k_to_iterators =
+          std::make_shared<k_to_iterators_t>(k_to_iterators_t(*k_to_iterators));
+    }
+  }
 };
 
 
---
+++ notes.md
@@ -0,0 +1,7 @@
+TODO
+1) wybrać strukturę
+2) impl
+3) exception safety
+4) przetestować wszystkie możliwe ścieżki rzucanych wyjątków
+5) jak mamy zapewniać semantykę copy-on-write jeśli nic nie modyfikujemy
+6) czy copy-one-write ma być tylko jak modyfikuje się kolejkę
