2ced0794cfffa202f4d92f5d445db36c469390a6
ryszmar <31414930+ryszmar@users.noreply.github.com>
2022-12-22 12:53:30 +0100

    bug fixes

--- kvfifo.h
+++ kvfifo.h
@@ -15,8 +15,8 @@
 template <typename K, typename V> 
 class kvfifo{
 public:
-  using queue_t = std::list<std::pair<K*, V>>;
-  using it_t = queue_t::iterator;
+  using queue_t = std::list<std::pair<const K*, V>>;
+  using it_t = typename queue_t::iterator;
   using deque_it_t = std::deque<it_t>;
   using k_to_iterators_t = std::map<K, deque_it_t>;
   
@@ -24,12 +24,12 @@ public:
 
   class k_iterator : std::iterator<std::bidirectional_iterator_tag, K> {
   public:
-    using difference_type = k_to_iterators_t::iterator::difference_type;
+    using difference_type = typename k_to_iterators_t::iterator::difference_type;
     using value_type = K;
 
     k_iterator() = default;
 
-    k_iterator(k_to_iterators_t::iterator it) : it(it) { }
+    k_iterator(typename k_to_iterators_t::iterator it) : it(it) { }
     k_iterator& operator++() {
       ++it;
       return *this;
@@ -64,7 +64,7 @@ public:
       return it->first;
     }
   private:
-    k_to_iterators_t::iterator it;
+    typename k_to_iterators_t::iterator it;
   };
 
   kvfifo() : data(std::make_shared<data_t>()) { }
@@ -144,45 +144,52 @@ public:
   std::pair<K const &, V &> first(K const &key) {
     Guard g(data);
 
-    auto queue_it = g.data->k_to_iterators->find(key);
-    if (queue_it == g.data->k_to_iterators->end()) {
+    auto queue_it = g.data->k_to_iterators.find(key);
+    if (queue_it == g.data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
 
     g.data->given_reference = true;
     g.accept();
-    
-    return deref(queue_it->front());
+    const K k = *(*(queue_it->second).begin())->first;
+    V v = (*(queue_it->second).begin())->second;
+    return std::pair<K const &, V &>(k, v);
   }
 
   std::pair<K const &, V const &> first(K const &key) const {
-    auto queue_it = data->k_to_iterators->find(key);
-    if (queue_it == data->k_to_iterators->end()) {
+    auto queue_it = data->k_to_iterators.find(key);
+    if (queue_it == data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
-    return deref(queue_it->front());
+    const K k = *(*(queue_it->second).begin())->first;
+    V v = (*(queue_it->second).begin())->second;
+    return std::pair<K const &, V &>(k, v);
   }
 
   std::pair<K const &, V &> last(K const &key) {
     Guard g(data);
 
-    auto queue_it = g.data->k_to_iterators->find(key);
-    if (queue_it == g.data->k_to_iterators->end()) {
+    auto queue_it = g.data->k_to_iterators.find(key);
+    if (queue_it == g.data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
 
     g.data->given_reference = true;
     g.accept();
 
-    return deref(queue_it->back());
+    const K k = *(queue_it->second).back()->first;
+    V v = (queue_it->second).back()->second;
+    return std::pair<K const &, V &>(k, v);
   }
 
   std::pair<K const &, V const &> last(K const &key) const {
-    auto queue_it = data->k_to_iterators->find(key);
-    if (queue_it == data->k_to_iterators->end()) {
+    auto queue_it = data->k_to_iterators.find(key);
+    if (queue_it == data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
-    return deref(queue_it->back());
+    const K k = *(queue_it->second).back()->first;
+    V v = (queue_it->second).back()->second;
+    return std::pair<K const &, V &>(k, v);
   }
   
   size_t size() const noexcept {
@@ -194,11 +201,11 @@ public:
   }
   
   size_t count(K const &k) const {
-    auto queue_it = data->k_to_iterators->find(k);
-    if (queue_it == data->k_to_iterators->end()) {
+    auto queue_it = data->k_to_iterators.find(k);
+    if (queue_it == data->k_to_iterators.end()) {
       return 0;
     }
-    return queue_it->size();
+    return queue_it->second.size();
   }
 
   void clear() {
@@ -206,11 +213,11 @@ public:
   }
 
   k_iterator k_begin() const noexcept {
-    return data->queue->begin();
+    return data->queue.begin();
   } 
 
   k_iterator k_end() const noexcept {
-    return data->queue->end();
+    return data->queue.end();
   }
 private:
   static_assert(std::bidirectional_iterator<k_iterator>);
@@ -233,8 +240,7 @@ private:
         new_element.second.push_back(list_it);
         list_it->first = &(k_to_iterators.insert(new_element).first->first);
       } else {
-        size_t size = (*map_it).size();
-        (*map_it).push_back(list_it);
+        map_it->second.push_back(list_it);
         list_it->first = &(map_it->first);
       }
     }
@@ -248,15 +254,15 @@ private:
     if (queue.empty()) {
       throw std::invalid_argument("queue is empty");
     }
-    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(queue.front());
+    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(*(queue.front().first));
     if (map_it == k_to_iterators.end()) {
       throw std::invalid_argument("problem with finding key in map");
     }
-    it_t list_it = map_it->front();
-    if (map_it->size() == 1) {
+    it_t list_it = map_it->second.front();
+    if (map_it->second.size() == 1) {
       k_to_iterators.erase(map_it);
     } else {
-      map_it->erase(map_it->begin());
+      map_it->second.erase(map_it->second.begin());
     }
     queue.erase(list_it);
   }
@@ -264,23 +270,23 @@ private:
   void pop(K const &k) {
     typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
     if (map_it == k_to_iterators.end()) {
-      throw std::invalid_argument();
+      throw std::invalid_argument("this key doesn't exist");
     }
-    it_t list_it = map_it->front();
-    if (map_it->size() == 1) {
+    it_t list_it = map_it->second.front();
+    if (map_it->second.size() == 1) {
       k_to_iterators.erase(map_it);
     } else {
-      map_it->erase(map_it->begin());
+      map_it->second.erase(map_it->second.begin());
     }
-    data->queue.erase(list_it);
+    queue.erase(list_it);
   }
 
   void move_to_back(K const &k) {
-    typename k_to_iterators_t::iterator map_it = k_to_iterators->find(k);
-    if (map_it == k_to_iterators->end()) {
+    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
+    if (map_it == k_to_iterators.end()) {
       return;
     }
-    for (auto &it_in_list: *map_it) { /// maybe iterating through deque may throw exception
+    for (auto &it_in_list: map_it->second) { /// maybe iterating through deque may throw exception
       queue.splice(queue.end(), queue, it_in_list);
     }
   }
