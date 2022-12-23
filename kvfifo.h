dd1cc92ab0ba03512445de87b74ec0d2bf1c62c8
Alessandro Peria <alessandroperia@icloud.com>
2022-12-23 17:10:01 +0100

    Fixed deep copy issue

--- kvfifo.h
+++ kvfifo.h
@@ -2,36 +2,48 @@
 #define KVFIFO_H
 
 #include <concepts>
-#include <memory>
-#include <map>
-#include <queue>
 #include <deque>
-#include <utility>
-#include <list>
 #include <functional>
-#include <stdexcept>
-#include <iterator>
 #include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <stdexcept>
+#include <utility>
 
-template <typename K, typename V> 
-class kvfifo{
+template <typename K, typename V> class kvfifo {
 public:
-  using queue_t = std::list<std::pair<const K*, V>>;
+  void print() const {
+    int x = 0;
+    for (auto &mapit : data->k_to_iterators)
+      x += mapit.second.size();
+    std::cerr << "mapsize: " << x << "\n";
+    std::cerr << "queue:\n";
+    for (auto &kv : data->queue) {
+      std::cerr << *kv.first << " " << kv.second << std::endl;
+    }
+    std::cerr << "end " << std::endl;
+  }
+
+  using queue_t = std::list<std::pair<const K *, V>>;
   using it_t = typename queue_t::iterator;
   using deque_it_t = std::deque<it_t>;
   using k_to_iterators_t = std::map<K, deque_it_t>;
-  
-  // Create wrapper iterator for k_to_iterators_t iterator  
+
+  // Create wrapper iterator for k_to_iterators_t iterator
 
   class k_iterator : std::iterator<std::bidirectional_iterator_tag, K> {
   public:
-    using difference_type = typename k_to_iterators_t::iterator::difference_type;
+    using difference_type =
+        typename k_to_iterators_t::iterator::difference_type;
     using value_type = K;
 
     k_iterator() = default;
 
-    k_iterator(typename k_to_iterators_t::iterator it) : it(it) { }
-    k_iterator& operator++() {
+    k_iterator(typename k_to_iterators_t::iterator it) : it(it) {}
+    k_iterator &operator++() {
       ++it;
       return *this;
     }
@@ -42,7 +54,7 @@ public:
       return tmp;
     }
 
-    k_iterator& operator--() {
+    k_iterator &operator--() {
       --it;
       return *this;
     }
@@ -52,32 +64,33 @@ public:
       operator--();
       return tmp;
     }
-    
-    bool operator==(k_iterator const &other) const {
-      return it == other.it;
-    }
-    
-    bool operator!=(k_iterator const &other) const {
-      return it != other.it;
-    }
-    
-    K const & operator*() const {
-      return it->first;
-    }
+
+    bool operator==(k_iterator const &other) const { return it == other.it; }
+
+    bool operator!=(k_iterator const &other) const { return it != other.it; }
+
+    K const &operator*() const { return it->first; }
+
   private:
     typename k_to_iterators_t::iterator it;
   };
 
-  kvfifo() : data(std::make_shared<data_t>()) { }
+  kvfifo() : data(std::make_shared<data_t>()) {}
 
-  kvfifo(kvfifo const &other) : data(other.data->given_reference ? std::make_shared<data_t> (*(other.data)) : other.data) { 
-    if (other.data->given_reference) 
+  kvfifo(kvfifo const &other)
+      : data(other.data->given_reference
+                 ? std::make_shared<data_t>(*(other.data))
+                 : other.data) {
+    if (other.data->given_reference) {
+
+      std::cout << "copying in constructor" << std::endl;
       data->given_reference = false;
+    }
   }
 
-  kvfifo(kvfifo &&other) noexcept : data(std::move(other.data))  { }
+  kvfifo(kvfifo &&other) noexcept : data(std::move(other.data)) {}
 
-  kvfifo& operator=(kvfifo other) noexcept {
+  kvfifo &operator=(kvfifo other) noexcept {
     data = std::move(other.data);
     return *this;
   }
@@ -85,7 +98,7 @@ public:
   void push(K const &k, V const &v) {
     Guard g(data);
     g.data->push(k, v);
-    g.accept();  
+    g.accept();
   }
 
   void pop() {
@@ -151,7 +164,7 @@ public:
 
     return std::pair<K const &, V const &>(*(entry.first), entry.second);
   }
-  
+
   std::pair<K const &, V &> first(K const &key) {
     Guard g(data);
 
@@ -159,10 +172,10 @@ public:
     if (queue_it == g.data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
-    
-    const K k = *(*(queue_it->second).begin())->first;
+
+    K const &k = queue_it->first;
     V &v = (*(queue_it->second).begin())->second;
-    
+
     g.data->given_reference = true;
     g.accept();
 
@@ -174,8 +187,8 @@ public:
     if (queue_it == data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
-    const K k = *(*(queue_it->second).begin())->first;
-    V const & v = (*(queue_it->second).begin())->second;
+    K const &k = queue_it->first;
+    V const &v = (*(queue_it->second).begin())->second;
     return std::pair<K const &, V const &>(k, v);
   }
 
@@ -187,7 +200,7 @@ public:
       throw std::invalid_argument("key not found");
     }
 
-    const K &k = *(queue_it->second).back()->first;
+    K const &k = queue_it->first;
     V &v = (queue_it->second).back()->second;
 
     g.data->given_reference = true;
@@ -201,19 +214,16 @@ public:
     if (queue_it == data->k_to_iterators.end()) {
       throw std::invalid_argument("key not found");
     }
-    const K &k = *(queue_it->second).back()->first;
+    K const &k = queue_it->first;
     V &v = (queue_it->second).back()->second;
+
     return std::pair<K const &, V &>(k, v);
   }
-  
-  size_t size() const noexcept {
-    return data->queue.size();
-  }
-  
-  bool empty() const noexcept {
-    return size() == 0;
-  }
-  
+
+  size_t size() const noexcept { return data->queue.size(); }
+
+  bool empty() const noexcept { return size() == 0; }
+
   size_t count(K const &k) const {
     auto queue_it = data->k_to_iterators.find(k);
     if (queue_it == data->k_to_iterators.end()) {
@@ -222,17 +232,16 @@ public:
     return queue_it->second.size();
   }
 
-  void clear() {
-    data = std::make_shared<data_t>();
-  }
+  void clear() { data = std::make_shared<data_t>(); }
 
   k_iterator k_begin() const noexcept {
     return k_iterator(data->k_to_iterators.begin());
-  } 
+  }
 
   k_iterator k_end() const noexcept {
     return k_iterator(data->k_to_iterators.end());
   }
+
 private:
   static_assert(std::bidirectional_iterator<k_iterator>);
 
@@ -241,70 +250,94 @@ private:
     k_to_iterators_t k_to_iterators;
     bool given_reference = false;
 
-  void push(K const &k, V const &v) {
-    queue.push_back(std::pair<K*, V>(NULL, v));
-        
-    it_t list_it = std::prev(queue.end()); // throws only when container is empty which we have a guarantee it isn't i think
-    
-    try {
-      typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
+    void push(K const &k, V const &v) {
+      queue.push_back(std::pair<K *, V>(NULL, v));
 
-      if (map_it == k_to_iterators.end()) {
-        auto new_element = typename std::pair<K, std::deque<typename queue_t::iterator>>(k, std::deque<typename queue_t::iterator>());
-        new_element.second.push_back(list_it);
-        list_it->first = &(k_to_iterators.insert(new_element).first->first);
-      } else {
-        map_it->second.push_back(list_it);
-        list_it->first = &(map_it->first);
+      it_t list_it =
+          std::prev(queue.end()); // throws only when container is empty which
+                                  // we have a guarantee it isn't i think
+
+      try {
+        typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
+
+        if (map_it == k_to_iterators.end()) {
+          auto new_element =
+              typename std::pair<K, std::deque<typename queue_t::iterator>>(
+                  k, std::deque<typename queue_t::iterator>());
+          new_element.second.push_back(list_it);
+          list_it->first = &(k_to_iterators.insert(new_element).first->first);
+        } else {
+          map_it->second.push_back(list_it);
+          list_it->first = &(map_it->first);
+        }
+      } catch (const std::exception &e) {
+        queue.erase(list_it);
+        throw e;
       }
     }
-    catch (const std::exception& e) {
+
+    void pop() {
+      if (queue.empty()) {
+        throw std::invalid_argument("queue is empty");
+      }
+      typename k_to_iterators_t::iterator map_it =
+          k_to_iterators.find(*(queue.front().first));
+      if (map_it == k_to_iterators.end()) {
+        throw std::invalid_argument("problem with finding key in map");
+      }
+      it_t list_it = map_it->second.front();
+      if (map_it->second.size() == 1) {
+        k_to_iterators.erase(map_it);
+      } else {
+        map_it->second.erase(map_it->second.begin());
+      }
       queue.erase(list_it);
-      throw e;
     }
-  }
 
-  void pop() {
-    if (queue.empty()) {
-      throw std::invalid_argument("queue is empty");
+    void pop(K const &k) {
+      typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
+      if (map_it == k_to_iterators.end()) {
+        throw std::invalid_argument("this key doesn't exist");
+      }
+      it_t list_it = map_it->second.front();
+      if (map_it->second.size() == 1) {
+        k_to_iterators.erase(map_it);
+      } else {
+        map_it->second.erase(map_it->second.begin());
+      }
+      queue.erase(list_it);
     }
-    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(*(queue.front().first));
-    if (map_it == k_to_iterators.end()) {
-      throw std::invalid_argument("problem with finding key in map");
-    }
-    it_t list_it = map_it->second.front();
-    if (map_it->second.size() == 1) {
-      k_to_iterators.erase(map_it);
-    } else {
-      map_it->second.erase(map_it->second.begin());
-    }
-    queue.erase(list_it);
-  }
 
-  void pop(K const &k) {
-    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
-    if (map_it == k_to_iterators.end()) {
-      throw std::invalid_argument("this key doesn't exist");
-    }
-    it_t list_it = map_it->second.front();
-    if (map_it->second.size() == 1) {
-      k_to_iterators.erase(map_it);
-    } else {
-      map_it->second.erase(map_it->second.begin());
-    }
-    queue.erase(list_it);
-  }
+    void move_to_back(K const &k) {
+      typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
+      if (map_it == k_to_iterators.end()) {
+        return;
+      }
 
-  void move_to_back(K const &k) {
-    typename k_to_iterators_t::iterator map_it = k_to_iterators.find(k);
-    if (map_it == k_to_iterators.end()) {
-      return;
-    }
-    for (auto &it_in_list: map_it->second) { /// maybe iterating through deque may throw exception
-      queue.splice(queue.end(), queue, it_in_list);
-    }
-  }
+      for (auto const &it_in_list :
+           map_it
+               ->second) { /// maybe iterating through deque may throw exception
+        queue.splice(queue.end(), queue, it_in_list);
 
+        std::cerr << "moving " << *it_in_list->first << ' '
+                  << it_in_list->second << " to back" << std::endl;
+      }
+
+      // print queue after moving
+      for (auto const &it : queue) {
+        std::cerr << "after moving " << *(it.first) << ' ' << it.second
+                  << " to back" << std::endl;
+        std::cerr << queue.size();
+      }
+    }
+    data_t() = default;
+    // copy constructor 
+    data_t(data_t const &other) : given_reference(false) {
+      for (auto it : other.queue) {
+        this->push(*(it.first), it.second);
+      }
+      std::cerr <<"---------------deep copy----------------"<<std::endl;
+    }
   };
 
   struct Guard {
@@ -321,9 +354,7 @@ private:
       }
     }
 
-    void accept() {
-      swap(data, orig_data);
-    }
+    void accept() { swap(data, orig_data); }
   };
 
   std::shared_ptr<data_t> data;
